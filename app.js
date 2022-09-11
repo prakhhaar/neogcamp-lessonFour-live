110e25a944e56a5ba979695c381470791a45d5f7
Prakhar Sharma <prakhar.shrm202@gmail.com>
2022-09-11 22:58:24 +0530

    added comments

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#input");
 var outputDiv = document.querySelector("#output");
 
+// API call
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
+// Adding the input by the user to the URL
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
 
+// Sending error message to the user using alert box
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
 }
 
-
+// Handling the click
 function clickHandler() {
     var inputText = txtInput.value;
 
+    // Fetching the API response
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
